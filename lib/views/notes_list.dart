e9e5e7b66113be07ca64fe7f4488696eb41ee1ce
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-08-31 08:51:12 +0500

    day1 completed

--- lib/views/notes_list.dart
+++ lib/views/notes_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rest_api_notes_app/models/notes.dart';
+import 'package:rest_api_notes_app/views/notes_delete.dart';
 import 'package:rest_api_notes_app/views/notes_modify.dart';
 
 class NotesList extends StatelessWidget {
@@ -42,21 +43,43 @@ class NotesList extends StatelessWidget {
       ),
       body: ListView.separated(
           itemBuilder: (context, index) {
-            return ListTile(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NotesModify(noteID: notes[index].noteID),
-                    ));
+            return Dismissible(
+              key: ValueKey(notes[index].noteID),
+              direction: DismissDirection.startToEnd,
+              onDismissed: (direction) {},
+              confirmDismiss: (direction) async {
+                final result = await showDialog(
+                    context: context, builder: (context) => NotesDelete());
+                //print(result);
+                return result;
               },
-              title: Text(
-                notes[index].noteTitle,
-                style: TextStyle(color: Theme.of(context).primaryColor),
+              background: Container(
+                color: Colors.red,
+                padding: EdgeInsets.only(left: 16.0),
+                child: Align(
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
               ),
-              subtitle: Text(
-                  'Last edited on ${formatDateTime(notes[index].lastEditDateTime)}'),
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            NotesModify(noteID: notes[index].noteID),
+                      ));
+                },
+                title: Text(
+                  notes[index].noteTitle,
+                  style: TextStyle(color: Theme.of(context).primaryColor),
+                ),
+                subtitle: Text(
+                    'Last edited on ${formatDateTime(notes[index].lastEditDateTime)}'),
+              ),
             );
           },
           separatorBuilder: (context, index) =>

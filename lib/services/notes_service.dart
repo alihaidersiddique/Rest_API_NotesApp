fbec56fb27c4bad8eecdf5d1a211337ac6222a61
Muhammad Ali Haider <alihaidersiddique@hotmail.com>
2021-08-31 12:19:15 +0500

    data received from API completed

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,23 +1,31 @@
+import 'package:rest_api_notes_app/models/api_response.dart';
 import 'package:rest_api_notes_app/models/notes.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class NotesService {
-  List<Notes> getNotesList() {
-    return [
-      Notes(
-          noteID: '1',
-          noteTitle: 'Note 1',
-          createdDateTime: DateTime.now(),
-          lastEditDateTime: DateTime.now()),
-      Notes(
-          noteID: '2',
-          noteTitle: 'Note 2',
-          createdDateTime: DateTime.now(),
-          lastEditDateTime: DateTime.now()),
-      Notes(
-          noteID: '3',
-          noteTitle: 'Note 3',
-          createdDateTime: DateTime.now(),
-          lastEditDateTime: DateTime.now()),
-    ];
+  //
+  static const API = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
+  static const headers = {'apiKey': '76c01c91-7bb1-457b-bf7f-5035c2bcf1bf'};
+
+  Future<APIResponse<List<Notes>>> getNotesList() {
+    return http.get(API + '/notes' as Uri, headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <Notes>[];
+        for (var item in jsonData) {
+          final note = Notes(
+              noteID: item['noteID'],
+              noteTitle: item['noteTitle'],
+              createdDateTime: DateTime.parse(item['createDateTime']),
+              lastEditDateTime: DateTime.parse(item['latestEditDateTime']));
+          notes.add(note);
+        }
+        return APIResponse<List<Notes>>(data: notes);
+      }
+      return APIResponse<List<Notes>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => APIResponse<List<Notes>>(
+        error: true, errorMessage: 'An error occured'));
   }
 }
--- lib/views/notes_list.dart
+++ lib/views/notes_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rest_api_notes_app/models/api_response.dart';
 import 'package:rest_api_notes_app/models/notes.dart';
 import 'package:rest_api_notes_app/services/notes_service.dart';
 import 'package:rest_api_notes_app/views/notes_delete.dart';
@@ -13,7 +14,9 @@ class NotesList extends StatefulWidget {
 class _NotesListState extends State<NotesList> {
   NotesService get service => GetIt.I<NotesService>();
 
-  List<Notes> notes = [];
+  final APIResponse<List<Notes>> _apiResponse;
+
+  bool _isLoading = false;
 
   String formatDateTime(DateTime dateTime) {
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
@@ -21,68 +24,90 @@ class _NotesListState extends State<NotesList> {
 
   @override
   void initState() {
-    notes = service.getNotesList();
+    _fetchNotes();
     super.initState();
+  }
+
+  _fetchNotes() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _apiResponse = await service.getNotesList();
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('List of Notes')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => NotesModify(),
-              ));
-        },
-        child: Icon(Icons.add),
-      ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Dismissible(
-              key: UniqueKey(),
-              direction: DismissDirection.startToEnd,
-              onDismissed: (direction) {},
-              confirmDismiss: (direction) async {
-                final result = await showDialog(
-                    context: context, builder: (context) => NotesDelete());
-                //print(result);
-                return result;
-              },
-              background: Container(
-                color: Colors.red,
-                padding: EdgeInsets.only(left: 16.0),
-                child: Align(
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                  ),
-                  alignment: Alignment.centerLeft,
-                ),
-              ),
-              child: ListTile(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            NotesModify(noteID: notes[index].noteID),
-                      ));
-                },
-                title: Text(
-                  notes[index].noteTitle,
-                  style: TextStyle(color: Theme.of(context).primaryColor),
-                ),
-                subtitle: Text(
-                    'Last edited on ${formatDateTime(notes[index].lastEditDateTime)}'),
-              ),
-            );
+        appBar: AppBar(title: Text('List of Notes')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NotesModify(),
+                ));
           },
-          separatorBuilder: (context, index) =>
-              Divider(height: 1, color: Colors.green),
-          itemCount: notes.length),
-    );
+          child: Icon(Icons.add),
+        ),
+        body: Builder(
+          builder: (context) {
+            if (_isLoading) {
+              return CircularProgressIndicator();
+            }
+            if (_apiResponse.error) {
+              return Center(child: Text(_apiResponse.errorMessage as String));
+            }
+            return ListView.separated(
+                itemBuilder: (context, index) {
+                  return Dismissible(
+                    key: UniqueKey(),
+                    direction: DismissDirection.startToEnd,
+                    onDismissed: (direction) {},
+                    confirmDismiss: (direction) async {
+                      final result = await showDialog(
+                          context: context,
+                          builder: (context) => NotesDelete());
+                      //print(result);
+                      return result;
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Align(
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ),
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NotesModify(
+                                  noteID: _apiResponse.data![index].noteID),
+                            ));
+                      },
+                      title: Text(
+                        _apiResponse.data![index].noteTitle,
+                        style: TextStyle(color: Theme.of(context).primaryColor),
+                      ),
+                      subtitle: Text(
+                          'Last edited on ${formatDateTime(_apiResponse.data![index].lastEditDateTime)}'),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) =>
+                    Divider(height: 1, color: Colors.green),
+                itemCount: _apiResponse.data!.length);
+          },
+        ));
   }
 }
